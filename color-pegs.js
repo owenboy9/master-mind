3deab114cbbd90d656c8334c3f5f0ba7eee8c041
owenboy9 <owenkraus9@gmail.com>
2023-11-05 19:30:05 +0100

    i created the color pegs class but i also couldn't resist the temptation to find out how to make them appear as colored buttons on screen (asked chat gtp for guidance, and they seemed very confused) i needed to insert a whole <style> section in my html file to activate the colored buttons (created by the forEach loop in color-pegs.js)

--- color-pegs.js
+++ color-pegs.js
@@ -1,7 +1,17 @@
-export default class ColorPegs{
+export default class ColorPegs {
+  constructor() {
+    // An array of color names for the code pegs
+    this.codePegs = ["red", "yellow", "green", "blue", "orange", "purple"]
 
+    // Get the container element by its id
+    this.codePegsContainer = document.getElementById("codePegsContainer")
 
-
-
-  
+    // Create and append colored buttons
+    this.codePegs.forEach((color) => {
+      let button = document.createElement("button")
+      button.style.backgroundColor = color
+      button.className = "code-peg-button"
+      codePegsContainer.appendChild(button)
+    })
+  }
 }
