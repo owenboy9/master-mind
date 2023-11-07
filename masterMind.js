9780a8d5767e1a41c9a8ce12c893b66ba6900efe
owenboy9 <owenkraus9@gmail.com>
2023-11-07 10:38:59 +0100

    added import statements to main just in case

--- masterMind.js
+++ masterMind.js
@@ -1,3 +1,8 @@
+import ColorPegs from "./colorPegs"
+import CodeMaster from "./codeMaster"
+import CodeBreaker from "./codeBreaker"
+import MarkPegs from "./markPegs"
+import Board from "./board"
 import Game from "./game"
 
 new Game()
